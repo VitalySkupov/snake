9115254790907b39ec4200f23ee9bae18498e4a0
VitalySkupov <skupovvv@gmail.com>
2019-06-28 10:57:21 +0300

    Создал горизонтальную линию.

--- Snake/Snake/Program.cs
+++ Snake/Snake/Program.cs
@@ -15,37 +15,8 @@ namespace Snake
 			Point p2 = new Point (4, 5, '#');
 			p2.Draw ();
 
-			List<int> numList = new List<int> ();
-			numList.Add (0);
-			numList.Add (1);
-			numList.Add (2);
-
-			int x = numList [0];
-			int y = numList [1];
-			int z = numList [2];
-
-			foreach (int i in numList)
-			{
-				Console.WriteLine (i);
-			}
-
-			numList.RemoveAt (0);
-
-			List<Point> pList = new List<Point> ();
-			pList.Add (p1);
-			pList.Add (p2);
-
-			/*int x1 = 1;
-			int y1 = 3;
-			char sym1 = '*';
-
-			Draw (x1, y1, sym1);*/
-
-			/*int x2 = 4;
-			int y2 = 5;
-			char sym2 = '#';
-
-			Draw (x2, y2, sym2);*/
+			HorizontalLine line = new HorizontalLine (5, 10, 8, '+');
+			line.Drow ();
 
 			Console.ReadLine ();
 		}
